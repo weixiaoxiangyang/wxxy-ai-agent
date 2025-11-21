89ce0dfc08361c9bffb099cc04dbe0a10bc9bd67
wubo18 <3154768697@qq.com>
2025-11-21 16:52:16 +0800

    wxxy-ai-agent项目初始化

---
+++ src/main/java/com/wxxy/wxxyaiagent/WxxyAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.wxxy.wxxyaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxxyAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxxyAiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wxxy/wxxyaiagent/WxxyAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxxy.wxxyaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxxyAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
